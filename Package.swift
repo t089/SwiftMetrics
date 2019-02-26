20164dd7f590ea20cd8d52ff4c55fc9c218dcb70
Tobias Haeberle <tobias.haeberle@holidu.com>
2019-02-26 12:02:53 +0100

    remove kitura

--- Package.swift
+++ Package.swift
@@ -18,13 +18,7 @@
 import PackageDescription
 import Foundation
 
-var webSocketPackage: Package.Dependency
 
-if ProcessInfo.processInfo.environment["KITURA_NIO"] != nil {
-    webSocketPackage = .package(url: "https://github.com/IBM-Swift/Kitura-WebSocket.git", .exact("0.1.0-nio"))
-} else {
-    webSocketPackage = .package(url: "https://github.com/IBM-Swift/Kitura-WebSocket.git", from: "2.0.0")
-}
 
 let package = Package(
   name: "SwiftMetrics",
@@ -32,37 +26,13 @@ let package = Package(
         .library(
             name: "SwiftMetrics",
             targets: ["SwiftMetrics"]),
-        .library(
-            name: "SwiftMetricsKitura",
-            targets: ["SwiftMetricsKitura",
-                "SwiftBAMDC",
-                "SwiftMetricsBluemix",
-                "SwiftMetricsDash",
-                "SwiftMetricsREST",
-                "SwiftMetricsPrometheus"]),
-
-        .executable(name: "SwiftMetricsEmitSample", targets: ["SwiftMetricsEmitSample"]),
-        .executable(name: "SwiftMetricsCommonSample", targets: ["SwiftMetricsCommonSample"]),
     ],
   dependencies: [
     .package(url: "https://github.com/IBM-Swift/Kitura.git", from: "2.3.0"),
-    webSocketPackage,
     .package(url: "https://github.com/IBM-Swift/Swift-cfenv.git", from: "6.0.0"),
     .package(url: "https://github.com/RuntimeTools/omr-agentcore", .exact("3.2.4-swift4")),
   ],
   targets: [
       .target(name: "SwiftMetrics", dependencies: ["agentcore", "hcapiplugin", "envplugin", "cpuplugin", "memplugin", "CloudFoundryEnv"]),
-      .target(name: "SwiftMetricsKitura", dependencies: ["SwiftMetrics", "Kitura"]),
-      .target(name: "SwiftBAMDC", dependencies: ["SwiftMetricsKitura", "Kitura-WebSocket"]),
-      .target(name: "SwiftMetricsBluemix", dependencies: ["SwiftMetricsKitura","SwiftBAMDC"]),
-      .target(name: "SwiftMetricsDash", dependencies: ["SwiftMetricsBluemix"]),
-      .target(name: "SwiftMetricsREST", dependencies: ["SwiftMetricsKitura"]),
-      .target(name: "SwiftMetricsPrometheus", dependencies:["SwiftMetricsKitura"]),
-      .target(name: "SwiftMetricsCommonSample", dependencies: ["SwiftMetrics"],
-            path: "commonSample/Sources"),
-      .target(name: "SwiftMetricsEmitSample", dependencies: ["SwiftMetrics"],
-            path: "emitSample/Sources"),
-      .testTarget(name: "CoreSwiftMetricsTests", dependencies: ["SwiftMetrics"]),
-      .testTarget(name: "SwiftMetricsRESTTests", dependencies: ["SwiftMetricsREST"])
    ]
 )
